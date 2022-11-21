6707a4ba969af42545b1c0c6374226abd3c4d5fc
Herz721 <haozheli@usc.edu>
2022-11-21 10:21:41 -0800

    initial CSV_Combiner

---
+++ CSV_Combiner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import sys
+
+class CSV_Combiner:
+    def __init__(self, file_list):
+        print(1)
+    
+if __name__ == '__main__':
+    args = sys.argv
+    print(args)
+    CSV_Combiner(args)
+    
